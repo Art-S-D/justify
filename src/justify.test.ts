60163f43006e0670eef78519396f51d430683623
arthur <artstdenis@gmail.com>
2021-07-29 10:46:28 +0200

    add some function to justify a text as well as some unit tests

---
+++ src/justify.test.ts
@@ -0,0 +1,63 @@
+import justify, { wrapLine, fitToWidth } from "./justify";
+
+describe("wrapLine", () => {
+    it("should wrap one line", () => {
+        let res = wrapLine("123 456 789", 3);
+        expect(res).toStrictEqual(["123", "456", "789"]);
+
+        res = wrapLine("123 456 789", 5);
+        expect(res).toStrictEqual(["123", "456", "789"]);
+
+        res = wrapLine("123 456 789", 8);
+        expect(res).toStrictEqual(["123 456", "789"]);
+    });
+    it("should return an empty array on an empty string", () => {
+        const res = wrapLine("", 5);
+        expect(res).toStrictEqual([]);
+    });
+    it("should break words", () => {
+        let res = wrapLine("123456789", 8);
+        expect(res).toStrictEqual(["1234567-", "89"]);
+
+        res = wrapLine("123456789", 3);
+        expect(res).toStrictEqual(["12-", "34-", "56-", "789"]);
+    });
+});
+
+describe("fitToWidth", () => {
+    it("should not modify a line that already fits", () => {
+        let res = fitToWidth("abcdefgh", 4);
+        expect(res).toBe("abcdefgh");
+
+        res = fitToWidth("abc def", 7);
+        expect(res).toBe("abc def");
+    });
+    it("should add space between two words", () => {
+        let res = fitToWidth("123 456", 10);
+        expect(res).toBe("123    456");
+
+        res = fitToWidth("abcde fgh", 12);
+        expect(res).toBe("abcde    fgh");
+    });
+    it("should add space between multiple words", () => {
+        let res = fitToWidth("123 456 78", 14);
+        expect(res).toBe("123   456   78");
+
+        res = fitToWidth("abcd efg hijk l", 20);
+        expect(res).toBe("abcd   efg   hijk  l");
+    });
+});
+
+describe("justify", () => {
+    it("should justify a long text", () => {
+        const input = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla felis nisi, consectetur vel justo vel, dapibus lacinia erat. Morbi accumsan tempus mi nec lobortis. Nam lobortis rutrum ligula, at dapibus sem sodales sit amet. Donec a sapien nec magna aliquam pretium. Integer euismod felis sed rhoncus sodales. Sed lacus enim, pellentesque sit amet massa fringilla, vehicula maximus felis. Integer quis diam ut lectus dignissim finibus quis sed turpis. Nullam id eros ut nulla sodales rhoncus. Sed convallis pretium pretium. Ut tincidunt augue sed sodales interdum. Sed neque nunc, venenatis vitae odio non, congue egestas sem. Vivamus ante turpis, ultricies ut ex in, mattis porttitor erat. Quisque pharetra dolor eros, at hendrerit nisl luctus sed. Morbi interdum sagittis placerat. Vivamus id commodo nibh.
+Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In hac habitasse platea dictumst. Praesent lacinia elit egestas lectus porttitor accumsan. Nulla finibus molestie consequat. Nulla rutrum blandit diam a dignissim. In hac habitasse platea dictumst. Proin rutrum vel mauris sed maximus.
+`;
+        const result = justify(input, 40);
+        for (const line of result.split("\n")) {
+            expect(line.length).toBe(40);
+            expect(line.charCodeAt(0)).not.toBe(" ");
+            expect(line.charCodeAt(39)).not.toBe(" ");
+        }
+    });
+});
---
+++ src/justify.ts
@@ -0,0 +1,96 @@
+/**
+ * take a string and split in strings of max size *width*
+ * it does so by adding line breaks between words
+ * and splits words if they do not fit
+ *
+ * assumes the line doesn't have extra spaces
+ *
+ * @param  {string} line the line to wrap
+ * @param  {number} width
+ * @returns string[]
+ */
+export function wrapLine(line: string, width: number): string[] {
+    const words = line.split(" ").reverse();
+    if (words.length === 0) return [];
+
+    let result = [];
+    let currentLine = null;
+
+    while (words.length > 0) {
+        const word = words.pop()!;
+
+        // here we make sure that the word can fit in *width* characters
+        // if not, we split in two
+        if (word.length > width) {
+            words.push(word.slice(width - 1));
+            words.push(word.slice(0, width - 1) + "-");
+            continue;
+        }
+
+        if (!currentLine) {
+            currentLine = word;
+            continue;
+        }
+
+        if (currentLine.length + word.length + 1 <= width) {
+            currentLine = `${currentLine} ${word}`;
+        } else {
+            result.push(currentLine);
+            currentLine = word;
+        }
+    }
+    if (currentLine?.length) result.push(currentLine);
+    return result;
+}
+
+/**
+ * take a line and add spaces between words to fit *width* characters
+ * assumes the line doesn't have extra spaces
+ *
+ * @param  {string} line the line to fit
+ * @param  {number} width
+ * @returns string
+ */
+export function fitToWidth(line: string, width: number): string {
+    let words = line.split(" ");
+    if (line.length >= width) return line;
+    const missingSpaces = width - line.length;
+
+    // we need to add at least *minSpacesToAdd* to every word to reach *width* characters
+    const minSpacesToAdd = Math.floor(missingSpaces / (words.length - 1));
+    if (minSpacesToAdd)
+        for (let i = 0; i < words.length - 1; i++)
+            words[i] += " ".repeat(minSpacesToAdd);
+
+    // if there are some spaces left to add after adding *minSpacesToAdd* to every word
+    // we add one space to the first *spacesLeft* words
+    const spacesLeft = missingSpaces - minSpacesToAdd * (words.length - 1);
+    for (let i = 0; i < spacesLeft; i++) {
+        words[i] += " ";
+    }
+    return words.join(" ");
+}
+
+/**
+ * justifies a text
+ *
+ * @param  {string} input
+ * @param  {number=80} width
+ * @returns string
+ */
+export default function justify(input: string, width: number = 80): string {
+    const lines = input.split(/\r?\n/);
+
+    let result = "";
+
+    for (const line of lines) {
+        const trimmed = line.replace(/ +/g, " ").trim();
+        if (trimmed.length === 0) continue;
+
+        for (const wrappedLine of wrapLine(trimmed, width)) {
+            const fitted = fitToWidth(wrappedLine, width);
+            result = result.length ? `${result}\n${fitted}` : fitted;
+        }
+    }
+    return result;
+}
