b461be340f849d0044f57a9dc6996ef005775a77
arthur <artstdenis@gmail.com>
2021-07-29 20:56:10 +0200

    add jwt authentication

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,52 @@
 import express from "express";
 import bodyParser from "body-parser";
+import jwt from "jsonwebtoken";
+import * as yup from "yup";
 
 import justify from "./justify";
 import env from "./envalid";
 
+const TokenSchema = yup.object({
+    email: yup.string().email(),
+});
+
 const app = express();
 app.use(bodyParser.text());
+app.use(bodyParser.json());
 
 app.post(
     "/api/justify",
     //TODO limite rate
-    //TODO check token
     (req, res) => {
-        if (!req.is("text/plain")) return res.sendStatus(400);
-        const justified = justify(req.body);
+        const token = req.header("bearer");
 
-        // Buffer.from prevents express from removing extra spaces
-        res.status(200).send(Buffer.from(justified));
+        if (!token) return res.sendStatus(401);
+
+        jwt.verify(token, env.APP_SECRET, (err /* , decoded */) => {
+            if (err) {
+                console.error(err);
+                return res.sendStatus(401);
+            }
+
+            if (!req.is("text/plain")) return res.sendStatus(400);
+            const justified = justify(req.body);
+
+            // Buffer.from prevents express from removing extra spaces
+            res.status(200).send(Buffer.from(justified));
+        });
+        res.sendStatus(400);
     }
 );
 
-app.get("/api/token", (req, res) => {
-    res.sendStatus(501);
+app.post("/api/token", async (req, res) => {
+    if (await TokenSchema.isValid(req.body)) {
+        const token = jwt.sign({ email: req.body.email }, env.APP_SECRET, {
+            expiresIn: "7 days",
+        });
+        res.status(200).json({ token });
+    } else {
+        res.sendStatus(400);
+    }
 });
 
 app.listen(env.APP_PORT, () => {
