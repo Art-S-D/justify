4aeebf90bba142039c15debbbe048161b9d56ffb
arthur <artstdenis@gmail.com>
2021-07-29 11:41:07 +0200

    make the /api/justify endpoint send back justified text

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,27 @@
 import express from "express";
+import bodyParser from "body-parser";
+
+import justify from "./justify";
 
 const app = express();
+app.use(bodyParser.text());
 
-app.post("/api/justify", (req, res) => {
-    // TODO
-    res.sendStatus(501);
-});
+app.post(
+    "/api/justify",
+    //TODO limite rate
+    (req, res) => {
+        if (!req.is("text/plain")) return res.sendStatus(400);
+        const justified = justify(req.body);
+
+        // Buffer.from prevents express from removing extra spaces
+        res.status(200).send(Buffer.from(justified));
+    }
+);
 
 app.get("/api/token", (req, res) => {
     res.sendStatus(501);
 });
 
-app.listen(8000);
-console.log("app listening on port 8000");
+app.listen(8000, () => {
+    console.log("app listening on port 8000");
+});
